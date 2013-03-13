c6d97314dbb3dd2cc642a236a8f790536a8990a9
Marcus Bauer <mabako@gmail.com>
2013-03-13 20:45:12 +0100

    validation for emails, names and passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   attr_accessible :name, :email, :password, :password_confirmation, :as => :create
 
-  validates_uniqueness_of :name, :case_sensitive => false
-  validates_uniqueness_of :email, :case_sensitive => false
-  validates_presence_of :password, :on => :create
+  validates :name, :length => { :minimum => 3 }, :uniqueness => { :case_sensitive => false }
+  validates :email, :uniqueness => { :case_sensitive => false }, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+  validates :password, :presence => true, :length => { :minimum => 8 }, :on => :create
 end
