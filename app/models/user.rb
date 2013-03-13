874697d5a139bc9c256f64e39e0dbc18e487f9b7
Marcus Bauer <mabako@gmail.com>
2013-03-13 19:22:26 +0100

    user model with name, email & password

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  attr_accessible :name, :email, :password, :as => :create
+
+  validates_uniqueness_of :name, :case_sensitive => false
+  validates_uniqueness_of :email, :case_sensitive => false
+  validates_presence_of :password, :on => :create
+end
