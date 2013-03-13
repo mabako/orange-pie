cde7940c52df2adcabe3ce667b85ff41f487b46b
Marcus Bauer <mabako@gmail.com>
2013-03-13 19:19:42 +0100

    base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kitten::Application.config.secret_token = '1fb8848468991eb57911955accbc9da167751337beeae5e3beae975f52c2b7d081ff0b6ddecb8b757702b3167273ce385a409e00d3846e1df46573fc8f0d4bba'
