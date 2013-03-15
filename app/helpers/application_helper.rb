a5cf1ee6d0184d1aa698a60c3cc2ca860f928f0a
Marcus Bauer <mabako@gmail.com>
2013-03-15 13:01:27 +0100

    more fancy formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def nav_active? page
     'active' if params[:controller] == page
   end
+
+  # returns a fancy string for dates:
+  def timely time
+    return "#{time_ago_in_words(time)} ago" if time > 30.days.ago
+    "#{time.strftime("%B %Y")}"
+  end
 end
