bedbcf34df69da09709cbd946885e74ff8515078
Marcus Bauer <mabako@gmail.com>
2013-03-15 14:04:18 +0100

    tweaked some code

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,12 @@
 class BlogsController < ApplicationController
+  before_filter :load_blog, :except => [:index, :new, :create]
+
   def index
     authorize! :read, Blog
     @blogs = Blog.sorted.all
   end
 
   def show
-    @blog = Blog.find(params[:id])
     authorize! :read, @blog
   end
 
@@ -27,35 +28,28 @@ class BlogsController < ApplicationController
   end
 
   def edit
-    @blog = Blog.find(params[:id])
     authorize! :update, @blog
   end
 
   def update
-    @blog = Blog.find(params[:id])
     authorize! :update, @blog
 
-    respond_to do |format|
-      if @blog.update_attributes(params[:blog])
-        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
-      end
+    if @blog.update_attributes(params[:blog])
+      redirect_to @blog, notice: 'Blog was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
-  # DELETE /blogs/1
-  # DELETE /blogs/1.json
   def destroy
-    @blog = Blog.find(params[:id])
     authorize! :destroy, @blog
     @blog.destroy
 
-    respond_to do |format|
-      format.html { redirect_to blogs_url }
-      format.json { head :no_content }
-    end
+    redirect_to blogs_url
+  end
+
+  private
+  def load_blog
+    @blog = Blog.find(params[:id])
   end
 end
