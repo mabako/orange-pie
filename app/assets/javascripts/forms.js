6e30e7e93b1b3c1fc7dfdb477321b66fb48bc3e1
Marcus Bauer <mabako@gmail.com>
2013-03-18 22:34:06 +0100

    js tweak to fix some wonky autologin stuff with chrome: if you deleted your old login name, it wouldnt correctly update the password field

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -6,13 +6,21 @@
     var input = $(this);
     setTimeout(function() {
       $('label', input.parent()).toggle(!input.val());
+
+      // autocomplete fix thing
+      var pass = $('input[type=password]');
+      if(pass.length >= 1) {
+        for(var i = 0; i < pass.length; ++ i) {
+          $('label', $(pass[i]).parent()).toggle(!$(pass[i]).val());
+        }
+      }
     }, 0);
   };
 
   $(function() {
     $('input, textarea').each(function() { toggleLabel.call(this); });
 
-    $('input, textarea').on('cut', toggleLabel).on('paste', toggleLabel).keydown(toggleLabel);
+    $('input, textarea').on('cut', toggleLabel).on('paste', toggleLabel).change(toggleLabel).keydown(toggleLabel);
     $('select').on('change', toggleLabel);
   });
 
