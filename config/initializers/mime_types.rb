98734f0f8d4dc8dbb71ef71493a2afecf79904b4
Marcus Bauer <mabako@gmail.com>
2013-03-20 01:18:13 +0100

    Disable the default XML Parser

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,5 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
+# Nobody wants XML, we got JSON. As nobody is really dependent on any existing XML-based structure, it shouldn't really bother anyone either.
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
+
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
