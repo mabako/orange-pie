5b2d1f197d91559a6fbf2eb23b637ccec9a92b55
Marcus Bauer <mabako@gmail.com>
2013-03-20 20:01:36 +0100

    fancier registration form, albeit it may need some work on how to display error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   attr_accessible :name, :email, :password, :password_confirmation, :as => :create
 
-  validates :name, :length => { :minimum => 3 }, :uniqueness => { :case_sensitive => false }
+  validates :name, :length => { :minimum => 3, :maximum => 16 }, :uniqueness => { :case_sensitive => false }
   validates :email, :uniqueness => { :case_sensitive => false }, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
   validates :password, :presence => true, :length => { :minimum => 8 }, :on => :create
 
