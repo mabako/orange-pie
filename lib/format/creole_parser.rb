9973e87f5903414438b3e378433fa3a96c2f67ad
Marcus Bauer <mabako@gmail.com>
2013-03-23 20:48:14 +0100

    emoticons

--- lib/format/creole_parser.rb
+++ lib/format/creole_parser.rb
@@ -11,5 +11,76 @@ module Format
     def make_headline level, text
       super_headline([4, level + 1].min, text)
     end
+
+    # emoticon support, the hacky way
+    def make_emoticon image, alt
+      '<img src="' << escape_html("/emoticons/#{image}.png") << '" alt="' << escape_html(alt) << '" title="' << escape_html(alt) << '"/>'
+    end
+    
+    alias :super_parse_inline_tag :parse_inline_tag
+
+    def parse_inline_tag str
+      # original
+      case str
+      when /\A\{\{\{(.*?\}*)\}\}\}/
+        @out << '<tt>' << escape_html($1) << '</tt>'
+      when /\A\{\{\s*(.*?)\s*(\|\s*(.*?)\s*)?\}\}/
+        if uri = make_image_link($1)
+          @out << make_image(uri, $3)
+        else
+          @out << escape_html($&)
+        end
+
+      # custom!
+      when /\A\:\)/ # :)
+        @out << make_emoticon('smiling', $&)
+      when /\A>\:\(/ # >:(
+        @out << make_emoticon('angry', $&)
+      when /\A\:\(/ # :(
+        @out << make_emoticon('frowning', $&)
+      when /\A\:x/ # :x
+        @out << make_emoticon('lips_sealed', $&)
+      when /\A\:(p|P)/ # :P :p
+        @out << make_emoticon('tongue_out', $&)
+      when /\A\:\|/ # :|
+        @out << make_emoticon('speechless', $&)
+      when /\A(D\:|\:@)/ # D: :@
+        @out << make_emoticon('terrified', $&)
+      when /\Ao\.O/ # o.O
+        @out << make_emoticon('surprised', $&)
+      when /\AO\.o/ # O.o
+        @out << make_emoticon('surprised_2', $&)
+      when /\A\:3/ # :3
+        @out << make_emoticon('aww', $&)
+      when /\A\:(S|s)/ # :S :s
+        @out << make_emoticon('confused', $&)
+      when /\A\:O/ # :O
+        @out << make_emoticon('gasping', $&)
+      when /\A\:D/ # :D
+        @out << make_emoticon('grinning', $&)
+      when /\A<3/ # <3
+        @out << make_emoticon('heart', $&)
+      when /\A>_</ # >_<
+        @out << make_emoticon('irritated', $&)
+      when /\A>\.</ # >.<
+        @out << make_emoticon('irritated_2', $&)
+      when /\AxD/ # xD
+        @out << make_emoticon('laughing', $&)
+      when /\AxP/ # xP
+        @out << make_emoticon('tongue_out_laughing', $&)
+      when /\A\:\// # :/
+        @out << make_emoticon('unsure', $&)
+      when /\A\:\\/ # :\
+        @out << make_emoticon('unsure_2', $&)
+      when /\A;\)/ # ;)
+        @out << make_emoticon('winking', $&)
+      when /\A\;D/ # ;D
+        @out << make_emoticon('winking_grinning', $&)
+      when /\A\;P/ # ;P
+        @out << make_emoticon('winking_tongue_out', $&)
+      else
+        super_parse_inline_tag str
+      end
+    end
   end
 end
--- lib/format/has_formatted.rb
+++ lib/format/has_formatted.rb
@@ -1,28 +1,26 @@
 class << ActiveRecord::Base
   # To use any of this, add a 'has_formatted :attributename' to the desired class.
-  def has_formatted *attributes
-    attributes.each do |attribute|
-      name = attribute.to_sym
+  def has_formatted attribute, options = {}
+    name = attribute.to_sym
 
-      attr_proc = lambda do
-        # retrieve the original text
-        text = self.send(name)
+    attr_proc = lambda do
+      # retrieve the original text
+      text = self.send(name)
 
-        # We could add other and/or multiple formatting options here some time in the future
-        # Creole will mostly do for now.
+      # We could add other and/or multiple formatting options here some time in the future
+      # Creole will mostly do for now.
 
-        # However, there should be a support for internal links, i.e.
-        #   [[Character:Diana Carvelli]] --> opens the according character page
-        #   [[Wiki:San Fierro]] --> opens the according Wiki page
-        #   [[Topic:id]] --> opens the according forum topic
-        #   [[Blog:id]]
-        # Likewise [[Character:...|Alternative Name]] should work
+      # However, there should be a support for internal links, i.e.
+      #   [[Character:Diana Carvelli]] --> opens the according character page
+      #   [[Wiki:San Fierro]] --> opens the according Wiki page
+      #   [[Topic:id]] --> opens the according forum topic
+      #   [[Blog:id]]
+      # Likewise [[Character:...|Alternative Name]] should work
 
-        Format::CreoleParser.new(text, :allowed_schemes => %w(http https)).to_html
-      end
-
-      # Creates a method of whatever options we got, prefixing it with formatted_
-      self.send(:define_method, "formatted_#{name}", &attr_proc)
+      Format::CreoleParser.new(text, :allowed_schemes => %w(http https)).to_html
     end
+
+    # Creates a method of whatever options we got, prefixing it with formatted_
+    self.send(:define_method, "formatted_#{name}", &attr_proc)
   end
 end
